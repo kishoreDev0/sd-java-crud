5f7f8a80f21257ea2b1e24ccc82fc1509b7a6227
kishore kumaran <142519697+kishoreDev0@users.noreply.github.com>
2026-04-08 11:53:18 +0530

    Updated codebase

--- src/main/java/com/ta/crud/Vehicle/Controller/VehicleController.java
+++ src/main/java/com/ta/crud/Vehicle/Controller/VehicleController.java
@@ -1,18 +1,14 @@
 package com.ta.crud.Vehicle.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import java.util.*;
-
 import com.ta.crud.Generic.GenericResponse;
 import com.ta.crud.Vehicle.Entity.Vehicle;
 import com.ta.crud.Vehicle.Service.VehicleService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
-
+@Tag(name = "Vehicle API", description = "Operations related to vehicles")
 @RestController
 @RequestMapping("/vehicle")
 public class VehicleController {
