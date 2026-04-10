9474d278c67732ef1b9c8d0217cf2ad62daa561d
kishore kumaran <142519697+kishoreDev0@users.noreply.github.com>
2026-04-10 14:21:51 +0530

    Update RoleService.java

--- src/main/java/com/ta/crud/Role/Service/RoleService.java
+++ src/main/java/com/ta/crud/Role/Service/RoleService.java
@@ -1,7 +1,6 @@
 package com.ta.crud.Role.Service;
 
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
